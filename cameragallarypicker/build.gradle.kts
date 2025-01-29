4d86da057eb3db451a17e956a666082928915dd0
Anuj Soni <anuj.soni@yash.com>
2025-01-29 16:10:50 +0530

    target

--- cameragallarypicker/build.gradle.kts
+++ cameragallarypicker/build.gradle.kts
@@ -39,23 +39,23 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    java {
-        toolchain {
-            languageVersion = JavaLanguageVersion.of(17)        // << --- ADD This
-        }
-    }
-//===============================
-
-    java {
-        sourceCompatibility = JavaVersion.VERSION_17            // << --- ADD This
+        sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+//    java {
+//        toolchain {
+//            languageVersion = JavaLanguageVersion.of(17)        // << --- ADD This
+//        }
+//    }
+////===============================
+//
+//    java {
+//        sourceCompatibility = JavaVersion.VERSION_17            // << --- ADD This
+//        targetCompatibility = JavaVersion.VERSION_17
+//    }
 
 }
 
